65fabb05a80899d61b9efa1acf7bced77673f6ad
Julia Fong <Juliamzfong@gmail.com>
2020-03-30 21:37:27 -0500

    Add first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'directivesDeepDive';
+export class AppComponent {
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
